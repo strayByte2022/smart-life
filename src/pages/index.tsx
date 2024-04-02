c8a688290e252cc569242aeea33cd52966528114
l1nhx <linhhandsom@gmail.com>
2024-04-03 00:34:00 +0700

    feat(slider): add images to slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { List, Page, Icon, useNavigate } from "zmp-ui";
+import { Page, Swiper, Box, Text, useNavigate, Button } from "zmp-ui";
 import { useRecoilValue } from "recoil";
 import { userState } from "../state";
 
@@ -9,26 +9,50 @@ const HomePage: React.FunctionComponent = () => {
   const user = useRecoilValue(userState);
   const navigate = useNavigate();
   return (
-    <Page className="page">
-      <div className="section-container">
-        <UserCard user={user.userInfo} />
-      </div>
-      <div className="section-container">
-        <List>
-          <List.Item
-            onClick={() => navigate("/about")}
-            suffix={<Icon icon="zi-arrow-right" />}
-          >
-            <div>About</div>
-          </List.Item>
-          <List.Item
-            onClick={() => navigate("/user")}
-            suffix={<Icon icon="zi-arrow-right" />}
-          >
-            <div>User</div>
-          </List.Item>
-        </List>
-      </div>
+    <Page className="section-container">
+      
+      <Box
+        mt={6}
+        flex
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        
+      >
+        <Swiper autoplay duration={3000} loop className="">
+          <Swiper.Slide className="h-100">
+            <img
+              className="slide-img"
+              src="slider-art-1.png"
+              alt="slide-1"
+            />
+          </Swiper.Slide>
+          <Swiper.Slide>
+            <img
+              className="slide-img"
+              src="slider-art-2.png"
+              alt="slide-2"
+            />
+            <Text.Title >Manage your home by using your </Text.Title>
+            <Text.Title> phone everywhere</Text.Title>
+          </Swiper.Slide>
+          <Swiper.Slide>
+            <img
+              className="slide-img"
+              src="slider-art-3.png"
+              alt="slide-3"
+            />
+          </Swiper.Slide>
+          
+
+        </Swiper>
+
+      </Box>
+      <Box mt={2} flex justifyContent="space-between">
+        <Button size="small" type="neutral" variant="tertiary">Skip</Button>
+        <Button size="small" type="neutral" variant="tertiary">Next</Button>
+      </Box>
+      
     </Page>
   );
 };
