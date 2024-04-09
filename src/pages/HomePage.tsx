0c42ff97a7f7b0890f9ae84508a3694d374c27d7
l1nhx <linhhandsom@gmail.com>
2024-04-10 00:04:30 +0700

    feat(application): add bottom nav bar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,12 +4,13 @@ import { Page, Text, Avatar } from 'zmp-ui'
 import '../css/HomePage.css'
 import { userState } from "../state";
 import { useRecoilValue } from "recoil";
-import { BottomNavigationAction, Box, Button, Tab, Tabs, Typography } from '@mui/material';
+import { BottomNavigation, Box, Button, IconButton, Tab, Tabs, Typography } from '@mui/material';
 import HomeTabs from '../components/Home/HomeTabs';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { BedroomIcon, CustomIcon, GarageIcon, KidsRoomIcon, KitchenIcon, OfficeIcon, SofaIcon, TvIcon } from '../components/icons';
 import { FunctionalButton } from '../components/Home';
-import { Tv } from '@mui/icons-material';
+import { BarChart, Cyclone, Home, Settings } from '@mui/icons-material';
+
 
 
 
@@ -38,22 +39,41 @@ const HomePage: React.FunctionComponent = () => {
       </Box>
       {
         value === 0 ? <Box className='home-functions'>
-          
-          <FunctionalButton icon={<SofaIcon/>} deviceQuantity={3} roomName={'Living Room'}/>
-          <FunctionalButton icon={<KitchenIcon/>} deviceQuantity={3} roomName={'Kitchen'}/>
-          <FunctionalButton icon={<></>} deviceQuantity={3} roomName={'Living Room'}/>
-          <FunctionalButton icon={<BedroomIcon/>} deviceQuantity={3} roomName={'Bedroom'}/>
-          <FunctionalButton icon={<GarageIcon/>} deviceQuantity={3} roomName={'Garage'}/>
-          <FunctionalButton icon={<OfficeIcon/>} deviceQuantity={3} roomName={'Office'}/>
-          <FunctionalButton icon={<KidsRoomIcon/>} deviceQuantity={3} roomName={'Kids Room'}/>
-          <FunctionalButton icon={<TvIcon/>} deviceQuantity={3} roomName={'TV Room'}/>
+
+          <FunctionalButton icon={<SofaIcon />} deviceQuantity={3} roomName={'Living Room'} />
+          <FunctionalButton icon={<KitchenIcon />} deviceQuantity={3} roomName={'Kitchen'} />
+          <FunctionalButton icon={<></>} deviceQuantity={3} roomName={'Living Room'} />
+          <FunctionalButton icon={<BedroomIcon />} deviceQuantity={3} roomName={'Bedroom'} />
+          <FunctionalButton icon={<GarageIcon />} deviceQuantity={3} roomName={'Garage'} />
+          <FunctionalButton icon={<OfficeIcon />} deviceQuantity={3} roomName={'Office'} />
+          <FunctionalButton icon={<KidsRoomIcon />} deviceQuantity={3} roomName={'Kids Room'} />
+          <FunctionalButton icon={<TvIcon />} deviceQuantity={3} roomName={'TV Room'} />
         </Box> : <Box>
           <Text.Title>
             under development
           </Text.Title>
         </Box>
       }
+      <BottomNavigation>
 
+        <Box  className='bottom-navigation-bar'>
+          <IconButton>
+            <Home />
+          </IconButton>
+
+          <IconButton>
+            <BarChart />
+          </IconButton>
+
+          <IconButton>
+            <Cyclone />
+          </IconButton>
+
+          <IconButton>
+            <Settings />
+          </IconButton>
+        </Box>
+      </BottomNavigation>
 
     </Page>
   )
