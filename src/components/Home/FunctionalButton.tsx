f41c1f04ec720f85476d0d35e9a8a333aac9ad0e
l1nhx <linhhandsom@gmail.com>
2024-04-10 21:37:39 +0700

    feat(home_page): add bottom drawer

--- src/components/Home/FunctionalButton.tsx
+++ src/components/Home/FunctionalButton.tsx
@@ -6,10 +6,11 @@ interface ButtonInterface{
     icon:React.ReactNode;
     roomName: string
     deviceQuantity: number
+    handleClick:void
 }
 const FunctionalButton = ({icon,roomName,deviceQuantity}) => {
     return (
-        <Button className='function-button' sx={{ backgroundColor: 'white' }} >
+        <Button className='function-button' sx={{ backgroundColor: 'white' }}>
             {icon}
             <Typography className='input-text'>{roomName}</Typography>
             <Typography className='input-text-low'>{`x${deviceQuantity} device(s)`}</Typography>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,15 @@
 import React, { useDeferredValue, useState } from 'react'
-import { Page, Text, Avatar } from 'zmp-ui'
+import { Page, Text, Avatar, Icon } from 'zmp-ui'
 
 import '../css/HomePage.css'
 import { userState } from "../state";
 import { useRecoilValue } from "recoil";
-import { BottomNavigation, Box, Button, IconButton, Tab, Tabs, Typography } from '@mui/material';
+import { BottomNavigation, Box, Button, Divider, IconButton, SwipeableDrawer, Tab, Tabs, Typography } from '@mui/material';
 import HomeTabs from '../components/Home/RoomDeviceTab';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { BedroomIcon, CustomIcon, GarageIcon, KidsRoomIcon, KitchenIcon, OfficeIcon, SofaIcon, TvIcon } from '../components/icons';
+import { BathrommIcon, BedroomIcon, CustomIcon, CustomPlusIcon, GarageIcon, KidsRoomIcon, KitchenIcon, OfficeIcon, SofaIcon, TvIcon } from '../components/icons';
 import { FunctionalButton } from '../components/Home';
-import { BarChart, Cyclone, Home, Settings } from '@mui/icons-material';
+import { BarChart, Cyclone, Home, KeySharp, Settings } from '@mui/icons-material';
 import RoomDeviceTab from '../components/Home/RoomDeviceTab';
 
 
@@ -17,7 +17,14 @@ import RoomDeviceTab from '../components/Home/RoomDeviceTab';
 
 const HomePage: React.FunctionComponent = () => {
   const [value, setValue] = React.useState(0);
-  
+  const [addDevice, setAddDevice] = React.useState(false);
+
+  const callAddDevice = () => {
+    setAddDevice(true);
+  }
+
+
+
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
@@ -25,56 +32,79 @@ const HomePage: React.FunctionComponent = () => {
   return (
     <Page className='home-page'>
       <Box className='content'>
-      <Box className='top-box' >
+        <Box className='top-box' >
 
-        <Box className='title-box'>
-          <Text.Title className='home-text'>Home</Text.Title>
-          <Box className='avatar'><Avatar online story='default' src='http://h5.zdn.vn/static/images/avatar.png' /></Box>
+          <Box className='title-box'>
+            <Text.Title className='home-text'>Home</Text.Title>
+            <Box className='avatar'><Avatar size={50} online story='default' src='http://h5.zdn.vn/static/images/avatar.png' /></Box>
 
+          </Box>
+          <Box className='tab-and-add-button'>
+
+            <RoomDeviceTab handleChange={handleChange} value={value} />
+            <IconButton onClick={callAddDevice}>
+              <CustomPlusIcon/>
+            </IconButton>
+          </Box>
         </Box>
-        <Box >
+        {
+          value === 0 ? <Box className='home-functions'>
 
-          <RoomDeviceTab handleChange={handleChange} value={value} />
-
-        </Box>
+            <FunctionalButton icon={<SofaIcon />} deviceQuantity={3} roomName={'Living Room'} />
+            <FunctionalButton icon={<KitchenIcon />} deviceQuantity={3} roomName={'Kitchen'} />
+            <FunctionalButton icon={<BathrommIcon/>} deviceQuantity={3} roomName={'Bathroom'} />
+            <FunctionalButton icon={<BedroomIcon />} deviceQuantity={3} roomName={'Bedroom'} />
+            <FunctionalButton icon={<GarageIcon />} deviceQuantity={3} roomName={'Garage'} />
+            <FunctionalButton icon={<OfficeIcon />} deviceQuantity={3} roomName={'Office'} />
+            <FunctionalButton icon={<KidsRoomIcon />} deviceQuantity={3} roomName={'Kids Room'} />
+            <FunctionalButton icon={<TvIcon />} deviceQuantity={3} roomName={'TV Room'} />
+          </Box> : <Box>
+            <Text.Title>
+              under development
+            </Text.Title>
+          </Box>
+        }
       </Box>
-      {
-        value === 0 ? <Box className='home-functions'>
 
-          <FunctionalButton icon={<SofaIcon />} deviceQuantity={3} roomName={'Living Room'} />
-          <FunctionalButton icon={<KitchenIcon />} deviceQuantity={3} roomName={'Kitchen'} />
-          <FunctionalButton icon={<></>} deviceQuantity={3} roomName={'Living Room'} />
-          <FunctionalButton icon={<BedroomIcon />} deviceQuantity={3} roomName={'Bedroom'} />
-          <FunctionalButton icon={<GarageIcon />} deviceQuantity={3} roomName={'Garage'} />
-          <FunctionalButton icon={<OfficeIcon />} deviceQuantity={3} roomName={'Office'} />
-          <FunctionalButton icon={<KidsRoomIcon />} deviceQuantity={3} roomName={'Kids Room'} />
-          <FunctionalButton icon={<TvIcon />} deviceQuantity={3} roomName={'TV Room'} />
-        </Box> : <Box>
-          <Text.Title>
-            under development
-          </Text.Title>
-        </Box>
-      }
+      <Box className='bottom-navigation-bar'>
+        <IconButton>
+          <Home />
+        </IconButton>
+
+        <IconButton>
+          <BarChart />
+        </IconButton>
+
+        <IconButton>
+          <Cyclone />
+        </IconButton>
+
+        <IconButton>
+          <Settings />
+        </IconButton>
       </Box>
+      <SwipeableDrawer anchor='bottom' open={addDevice} onClose={() => { setAddDevice(false) }} onOpen={() => { setAddDevice(true) }}>
+        <Box className='bottom-drawer'>
+          <Text.Title className='bottom-drawer-title'>Add New</Text.Title>
+          <Divider className='bottom-drawer-divider' />
 
-        <Box className='bottom-navigation-bar'>
-          <IconButton>
-            <Home />
-          </IconButton>
+          <Button className='bottom-drawer-buttons'>
+            <Box className='bottom-drawer-buttons-layout'>
+              <KeySharp />
+              <Text.Title>Add New Room</Text.Title>
+            </Box>
+          </Button>
 
-          <IconButton>
-            <BarChart />
-          </IconButton>
-      
-          <IconButton>
-            <Cyclone />
-          </IconButton>
+          <Button className='bottom-drawer-buttons'>
+            <Box className='bottom-drawer-buttons-layout'>
+              <KeySharp />
+              <Text.Title>Add New Device</Text.Title>
+            </Box>
+          </Button>
 
-          <IconButton>
-            <Settings />
-          </IconButton>
+
         </Box>
-      
+      </SwipeableDrawer>
 
     </Page>
   )
