7a088177d08dd070fc555db799a7fe81fad847a4
l1nhx <linhhandsom@gmail.com>
2024-04-16 20:05:54 +0700

    fix(home_page): fix add button size

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import { Page, Text, Avatar, Icon, useNavigate } from 'zmp-ui'
 import '../css/HomePage.css'
 import { userState } from "../state";
 import { useRecoilValue } from "recoil";
-import { BottomNavigation, Box, Button, Divider, IconButton, SwipeableDrawer, Tab, Tabs, Typography } from '@mui/material';
+import { BottomNavigation, Box, Button, Divider, IconButton, SvgIcon, SwipeableDrawer, Tab, Tabs, Typography } from '@mui/material';
 import HomeTabs from '../components/Home/RoomDeviceTab';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { BathrommIcon, BedroomIcon, CustomIcon, CustomPlusIcon, GarageIcon, KidsRoomIcon, KitchenIcon, OfficeIcon, SofaIcon, TvIcon } from '../components/icons';
@@ -43,7 +43,12 @@ const HomePage: React.FunctionComponent = () => {
 
             <RoomDeviceTab handleChange={handleChange} value={value} />
             <IconButton onClick={callAddDevice}>
-              <CustomPlusIcon />
+              <SvgIcon sx={{width:'5vh', height:'5vh'}}>
+                <svg width="10" height="10" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M17 34C26.3888 34 34 26.3888 34 17C34 7.61116 26.3888 0 17 0C7.61116 0 0 7.61116 0 17C0 26.3888 7.61116 34 17 34Z" fill="#EF3024" />
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M17 8C16.5858 8 16.25 8.33579 16.25 8.75V16.25H8.75C8.33579 16.25 8 16.5858 8 17C8 17.4142 8.33579 17.75 8.75 17.75H16.25V25.25C16.25 25.6642 16.5858 26 17 26C17.4142 26 17.75 25.6642 17.75 25.25V17.75H25.25C25.6642 17.75 26 17.4142 26 17C26 16.5858 25.6642 16.25 25.25 16.25H17.75V8.75C17.75 8.33579 17.4142 8 17 8Z" fill="white" />
+                </svg>
+              </SvgIcon>
             </IconButton>
           </Box>
         </Box>
@@ -86,11 +91,11 @@ const HomePage: React.FunctionComponent = () => {
       <SwipeableDrawer
         anchor='bottom'
         open={addDevice}
-        onClose={() => { setAddDevice(false)}}
-        onOpen={() => { setAddDevice(true)}}
-        sx={{borderTopLeftRadius:'20px', borderTopRightRadius:'20px', borderColor:'black  '}}
-        
-        >
+        onClose={() => { setAddDevice(false) }}
+        onOpen={() => { setAddDevice(true) }}
+        sx={{ borderTopLeftRadius: '20px', borderTopRightRadius: '20px', borderColor: 'black  ' }}
+
+      >
         <Box className='bottom-drawer'>
           <Text.Title className='bottom-drawer-title'>Add New</Text.Title>
           <Divider className='bottom-drawer-divider' />
