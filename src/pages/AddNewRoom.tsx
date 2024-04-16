b4251a3c0728ee6747f8c8c7704e5cd731a8c966
l1nhx <linhhandsom@gmail.com>
2024-04-17 00:48:00 +0700

    feat(add_room_page): add control buttons

--- src/pages/AddNewRoom.tsx
+++ src/pages/AddNewRoom.tsx
@@ -1,27 +1,49 @@
-import { ArrowBack } from '@mui/icons-material'
-import { AppBar, IconButton, Toolbar, Typography,Button } from '@mui/material'
+import { ArrowBack, KeyboardTab } from '@mui/icons-material'
+import { AppBar, IconButton, Toolbar, Typography, Button, Box, TextField, RadioGroup, Radio, FormControlLabel } from '@mui/material'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import {  Page } from 'zmp-ui'
-
+import { Page } from 'zmp-ui'
+import PageWithToolbar from '../components/PageWithToolbar'
+import { styled } from '@mui/material/styles';
+import { BathrommIcon, BedroomIcon, GarageIcon, KidsRoomIcon, KitchenIcon, OfficeIcon, SofaIcon, TvIcon } from '../components/icons'
+import CustomRadioButton from '../components/AddRoom/CustomRadioButton'
+import '../css/AddRoom.module.css'
+const CSSTextField = styled(TextField)
+{
+  //  later nhak :D 
+}
 const AddNewRoomPage = () => {
   const navigate = useNavigate()
   return (
-    <Page>
-      <AppBar position='static' >
-        <Toolbar sx={{ bgcolor: '#0A182E', display: 'flex', justifyContent: 'space-between' }}>
+    <Page className='add-room-page'>
+      <PageWithToolbar
+        title={"Add room"}
+        behavior={
+          <Typography>Save</Typography>
+        }
 
-          <IconButton onClick={() => { navigate(-1) }}>
-            <ArrowBack sx={{ color: 'white' }} />
+      />
+      <Box sx={{ paddingLeft: '20px', paddingRight: '20px', paddingTop: '34px' }}>
+        <Typography sx={{ color: 'grey' }}>Enter Room's Name</Typography>
+        <TextField sx={{ border: 'grey', paddingTop: '6px' }} />
+        <Box sx={{ paddingTop: '23px' }}>
+          <Typography>Select Room's Icon</Typography>
+          <Box>
+            <RadioGroup sx={{display:'flex', flexDirection:'row', columnCount:'1', flexWrap:'wrap'}}>
+              <CustomRadioButton controlIcon={<KitchenIcon/>} label="Kitchen"/>
+              <CustomRadioButton controlIcon={<BedroomIcon/>} label="Bed Room"/>
+              <CustomRadioButton controlIcon={<BathrommIcon/>} label="Bathroom"/>
+              <CustomRadioButton controlIcon={<OfficeIcon/>} label="Office"/>
+              <CustomRadioButton controlIcon={<TvIcon/>} label="TV Room"/>
+              <CustomRadioButton controlIcon={<SofaIcon/>} label="Living Room"/>
+              <CustomRadioButton controlIcon={<GarageIcon/>} label="Garage"/>
+              <CustomRadioButton controlIcon={<BathrommIcon/>} label="Toilet"/>
+              <CustomRadioButton controlIcon={<KidsRoomIcon/>} label="kid Room"/>
+            </RadioGroup>
+          </Box>
+        </Box>
+      </Box>
 
-          </IconButton>
-          <Typography>Add Device</Typography>
-          <Button sx={{ color: 'white', textTransform: 'initial' }}>
-            <Typography>Save</Typography>
-          </Button>
-        </Toolbar>
-
-      </AppBar>
     </Page>
   )
 }
