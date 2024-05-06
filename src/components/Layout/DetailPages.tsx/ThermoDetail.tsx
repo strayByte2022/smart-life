cb71fe5c08927eb4145c95cd4c78f6cf5486f233
Vương Hồng Lĩnh <linhhandsom@gmail.com>
2024-05-06 17:01:57 +0700

    feat: add ada data fetch

--- src/components/Layout/DetailPages.tsx/ThermoDetail.tsx
+++ src/components/Layout/DetailPages.tsx/ThermoDetail.tsx
@@ -1,7 +1,38 @@
 import { Box, Switch, Typography } from '@mui/material'
-import React from 'react'
+import { Gauge, gaugeClasses, GaugeReferenceArc, GaugeValueArc } from '@mui/x-charts'
+import React, { useEffect, useState } from 'react'
+import { Button } from 'zmp-ui'
 
 const ThermoDetail = () => {
+    const [temperature, setTemperature] = useState(0)
+
+    var myHeaders = new Headers();
+    myHeaders.append("X-AIO-Key", "aio_yGID980zvglJif4Ld8TCBTuZhaB4");
+    var requestOptions: Object = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+    const fetchTemperature = async () => {
+        const response = await fetch("https://io.adafruit.com/api/v2/phuc12082003/feeds/smarthome-temperature/data/retain", requestOptions)
+        const result = await response.text()
+        setTemperature(parseFloat(result))
+        console.log(result)
+    }
+
+    useEffect(() => {
+
+        const fetchInterval = setInterval(() => {
+            fetchTemperature()
+        }, 10000)
+        return () => clearInterval(fetchInterval)
+
+    }, [])
+    useEffect(() => {
+
+        fetchTemperature()
+        console.log("Run in first time")
+    }, [])
     return (
         <Box>
             <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -12,11 +43,35 @@ const ThermoDetail = () => {
 
                 <Switch />
             </Box>
-            <Box sx={{ paddingTop: '40px' }}>
-                <div>
+            <Box sx={{ display: 'flex', paddingTop: '40px' }}>
+                <Box>
+                    <Box sx={{ display: 'flex' }}>
+                        <Typography variant='h1' sx={{ color: temperature <=39?"#52b202":"red", }}>
+                            {temperature}
+                        </Typography>
+                        <Typography variant='h2'>° </Typography>
+                    </Box>
 
-                </div>
+                    <Typography>Temperatue, Celcius</Typography>
+                </Box>
+                <Gauge width={100} height={200} value={temperature} startAngle={0} endAngle={-180} cornerRadius="50%" 
+                    sx={(theme) => ({
+                        [`& .${gaugeClasses.valueText}`]: {
+                          fontSize: 40,
+                        },
+                        [`& .${gaugeClasses.valueArc}`]: {
+                          fill: temperature <=39?"#52b202":"red",
+                        },
+                        [`& .${gaugeClasses.referenceArc}`]: {
+                          fill: theme.palette.text.disabled,
+                        },
+                      })}
+                    >
+                    
+                    
+                </Gauge>
             </Box>
+
         </Box>
     )
 }
