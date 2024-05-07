877975711872721ceae3ae4d3c07ccfc3d345de4
Vương Hồng Lĩnh <linhhandsom@gmail.com>
2024-05-07 16:03:29 +0700

    fix: fix async state update

--- src/components/Layout/DetailPages.tsx/FansDetail.tsx
+++ src/components/Layout/DetailPages.tsx/FansDetail.tsx
@@ -1,44 +1,72 @@
 import { Box, Button, IconButton, Switch, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import Slider from '@mui/material/Slider';
-import { Icon } from 'zmp-ui';
-import { Add, PlusOne, Remove } from '@mui/icons-material';
+
+import { Add, Remove, Warning } from '@mui/icons-material';
+import { useSnackbar } from 'zmp-ui';
 const FansDetail = () => {
     function valuetext(value: number) {
         return `${value}RPM`;
     }
 
     const [fanspeed, setFanSpeed] = useState(0)
+    const { openSnackbar, setDownloadProgress, closeSnackbar } = useSnackbar();
     const myHeaders = new Headers();
     myHeaders.append("X-AIO-Key", "aio_yGID980zvglJif4Ld8TCBTuZhaB4");
     myHeaders.append("Content-Type", "application/json");
 
-    const raw = JSON.stringify({
-        "value": fanspeed
-    });
+
     const requestOptions: Object = {
         method: "GET",
         headers: myHeaders,
         redirect: "follow"
     };
-    const postOptions: Object = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow"
-    };
+
     const getFanSpeed = async () => {
-        const respone = await fetch("https://io.adafruit.com/api/v2/phuc12082003/feeds/smarthome-fanspeed/data/retain", requestOptions)
-        const result = await respone.text()
-        console.log(result)
-        setFanSpeed(parseFloat(result))
+        try {
+            const respone = await fetch("https://io.adafruit.com/api/v2/phuc12082003/feeds/smarthome-fanspeed/data/retain", requestOptions)
+            const result = await respone.text()
+            console.log(result)
+            setFanSpeed(parseFloat(result))
+        }
+        catch (error) {
+            console.log(error)
+        }
     }
-    const postFanSpeed = async () => {
 
-        const response = await fetch("https://io.adafruit.com/api/v2/phuc12082003/feeds/smarthome-fanspeed/data", postOptions);
-        const result = await response.text();
-        console.log("Post result: ", result)
+    const postFanSpeed = async (newSpeed) => {
 
+        const raw = JSON.stringify({
+            "value": newSpeed
+        });
+        const postOptions: Object = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+        try {
+            const response = await fetch("https://io.adafruit.com/api/v2/phuc12082003/feeds/smarthome-fanspeed/data", postOptions);
+            const result = await response.text();
+            console.log("Post result: ", result)
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+
+    }
+    const handleFanSpeedChange = async (newSpeed) => {
+        setFanSpeed(newSpeed);
+        postFanSpeed(newSpeed)
+
+    }
+    const handleOpenSnackBar = () => {
+        openSnackbar({
+            text: "Invalid fanspeed",
+            type: "warning",
+            duration: 5000,
+            prefixIcon: <Warning />
+        })
     }
     useEffect(() => {
         getFanSpeed()
@@ -47,31 +75,73 @@ const FansDetail = () => {
         <Box>
             <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
                 <Box>
-                    <Typography>Onlyfans</Typography>
+                    <Typography>Fanspeed: </Typography>
                     <Typography>{fanspeed}</Typography>
                 </Box>
 
                 <Switch />
             </Box>
-            <Box sx={{ display: 'grid', justifyContent: 'center', paddingTop: '40px', gap: '40px' }}>
-                <Box sx={{ display: 'flex', gap: '40px' }}>
-                    <Typography>±1</Typography>
-                    <IconButton sx={{ border: 'solid' }} onClick={(e) => { setFanSpeed(fanspeed - 1); postFanSpeed() }}>
-                        <Remove />
-                    </IconButton>
-                    <IconButton sx={{ border: 'solid' }} onClick={() => { setFanSpeed(fanspeed + 1); postFanSpeed() }}>
-                        <Add />
-                    </IconButton>
+            <Box sx={{ display: 'flex', justifyContent: 'center', paddingTop: '40px', gap: '40px', paddingRight: '20px' }}>
+                <Box sx={{ display: 'grid', gap: '20px', alignContent: 'center' }}>
+                    <Typography variant='h4' >
+                        ± 1
+                    </Typography>
+                    <Typography variant='h4'>
+                        ± 10
+                    </Typography>
                 </Box>
-                <Box sx={{ display: 'flex', gap: '40px' }}>
-                    <Typography>±10</Typography>
-                    <IconButton sx={{ border: 'solid' }} onClick={(e) => { setFanSpeed(fanspeed - 10); postFanSpeed() }}>
-                        <Remove />
-                    </IconButton>
-                    <IconButton sx={{ border: 'solid' }} onClick={() => { setFanSpeed(fanspeed + 10); postFanSpeed() }}>
-                        <Add />
-                    </IconButton>
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+                    <Box sx={{ display: 'flex', gap: '40px' }}>
+                        <IconButton sx={{ border: 'solid' }} onClick={() => {
+
+                            let newSpeed = fanspeed - 1
+                            if (newSpeed < 0) {
+                                handleOpenSnackBar()
+                                return
+                            }
+                            handleFanSpeedChange(newSpeed)
+                        }}>
+                            <Remove />
+                        </IconButton>
+                        <IconButton sx={{ border: 'solid' }} onClick={() => {
+                            let newSpeed = fanspeed + 1
+                            if (newSpeed > 100) {
+                                handleOpenSnackBar()
+                                return
+                            }
+                            handleFanSpeedChange(newSpeed)
+
+                        }}>
+                            <Add />
+                        </IconButton>
+                    </Box>
+                    <Box sx={{ display: 'flex', gap: '40px' }}>
+
+                        <IconButton sx={{ border: 'solid' }} onClick={() => {
+                            let newSpeed = fanspeed - 10
+                            if (newSpeed < 0) {
+                                handleOpenSnackBar()
+                                return
+                            }
+                            handleFanSpeedChange(newSpeed)
+
+                        }}>
+                            <Remove />
+                        </IconButton>
+                        <IconButton sx={{ border: 'solid' }} onClick={() => {
+                            let newSpeed = fanspeed + 10
+                            if (newSpeed > 100) {
+                                handleOpenSnackBar()
+                                return
+                            }
+                            handleFanSpeedChange(newSpeed)
+                        }}>
+                            <Add />
+                        </IconButton>
+                    </Box>
                 </Box>
+
+
 
             </Box>
         </Box>
